1c2e2e30058716f4ca94c24c384c45c4d587e085
usama-aaslam <cs.usama.aslam@gmail.com>
2023-12-24 17:04:03 +0500

    Part Two Assessment

--- src/Pages/LoginHomePage.php
+++ src/Pages/LoginHomePage.php
@@ -23,15 +23,32 @@ class HomePage
     
     public function deleteAccount()
     {
-        // Click on the three dots menu on the Navigation Bar
-        $this->driver->findElement(WebDriverBy::id('menuTrigger'))->click();
+        // Click on the  menu on the Navigation Bar
+        $this->driver->findElement(WebDriverBy::xpath('//*[@class="inline-b"]'))->click();
         
         // Select Account Settings
-        $this->driver->findElement(WebDriverBy::xpath('//div[@class="dropdown-menu"]/a[text()="Account Settings"]'))->click();
+        $this->driver->findElement(WebDriverBy::xpath('//*[@class="inline-b"]/a[text()="Account Settings"]'))->click();
         
-        // Additional steps to complete the deletion flow
-        // ...
-        
+        //  steps to complete the deletion flow
+         $this->driver->executeScript('window.scrollTo(0, document.body.scrollHeight);');
+         $this->driver->findElement(WebDriverBy::xpath('(//*[text() = 'Delete My Account'])[2]"]'))->click();
+         
+         $this->driver->findElement(WebDriverBy::xpath('(//*[text() = 'Delete My Account'])[2]"]'))->click();
+         
+         $this->driver->findElement(WebDriverBy::xpath('//button[@type="submit" and text() = 'Yes, Delete My Account']')->click();
+         
+         $expectedUrl = 'https://careers.bayt.com/en/delete-account-confirmation/';
+         
+         // The actual URL 
+         $actualUrl = $_SERVER['REQUEST_URI']; 
+         
+         // Assert that the actual URL is equal to the expected URL
+         assert($actualUrl === $expectedUrl);
+         
+         
+         // assert($actualUrl === $expectedUrl, 'URL does not match expected value');
+         
+         echo 'Account deleted successfully!'; // This will be printed if the assertion passes
         return new RegistrationPage($this->driver);
     }
 }
